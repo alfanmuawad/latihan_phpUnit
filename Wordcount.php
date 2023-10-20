6e440bc632db7dd34386dabc0571c6ea2097c833
mochalfannn <mochalfannn@gmail.com>
2023-10-20 12:48:30 +0700

    test

--- Wordcount.php
+++ Wordcount.php
@@ -1,6 +1,11 @@
 <?php
+// membuat class countWords
 class wordcount{
+    // membuat fungsi countword
     public function countWords($sentence){
+        // menembalikan count
+        // memecah string berdasarkan koma ( ). 
+        // Hasilnya adalah array yang berisi kata-kata yang terpisah
         return count(explode(" ",$sentence));
      }
 }
--- simpletest.php
+++ simpletest.php
@@ -1,8 +1,11 @@
 <?php
     use PHPUnit\Framework\TestCase;
+    // mengambil dari file Wordcount.php sebelumnya
     require_once "Wordcount.php";
+    // membuat class yang di extends dari testcase
     class Simpletest extends TestCase {
         public function testcountword (){
+            // membuat object baru
             $wc = new wordcount();
 
             $TestSentence = "My name is alfan";
